bf1bd52a92801f2d7c6935cd67b1289dbd1c4bfe
Aditya Kumar <way2adityakumar@gmail.com>
2025-10-08 21:35:59 +0530

    package.json

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 const parent = React.createElement("div", { id: "Parent" }, [
   React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "I am h1 tag"),
-    React.createElement("h2", {}, "I am h2 tag"),
+    React.createElement("h1", {id:"12"}, "I am h1 tag"),
+    React.createElement("h2", {id:"2"}, "I am h2 tag"),
   ]),
   React.createElement("div", { id: "child2" }, [
-    React.createElement("h1", {}, "I am h1 tag"),
-    React.createElement("h2", {}, "I am h2 tag"),
+    React.createElement("h1", {id:"3"}, "I am h1 tag"),
+    React.createElement("h2", {id:"4"}, "I am h2 tag"),
   ]),
 ]);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-
 root.render(parent);
