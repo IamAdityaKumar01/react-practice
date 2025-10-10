b9f606209c543d8e2b290a5287cf007f88bc8687
Aditya Kumar <way2adityakumar@gmail.com>
2025-10-10 12:59:59 +0530

    React component

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const parent = React.createElement("div", { id: "Parent" }, [
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {id:"12"}, "I am h1 tag"),
-    React.createElement("h2", {id:"2"}, "I am h2 tag"),
-  ]),
-  React.createElement("div", { id: "child2" }, [
-    React.createElement("h1", {id:"3"}, "I am h1 tag"),
-    React.createElement("h2", {id:"4"}, "I am h2 tag"),
-  ]),
-]);
+const Heading_ReactElem = () => <h1>React element Heading</h1>;
+
+const reactElem = <h3>I am heading 3</h3>;
+const Heading = () => (
+  <div className="Container">
+    <Heading_ReactElem />
+    {Heading_ReactElem()}
+    <h1>This is the heading made from react component</h1>
+    <h2>I am heading 2</h2>
+  </div>
+);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+
+root.render(<Heading />);
