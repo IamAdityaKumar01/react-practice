7eaa8a5527f2008e635253a050452cae6dbbf61a
Aditya Kumar <way2adityakumar@gmail.com>
2025-10-23 18:20:40 +0530

    componentDidMount added

--- src/components/About.js
+++ src/components/About.js
@@ -1,11 +1,23 @@
 import UserClass from "./UserClass";
+import { Component } from "react";
 
-const About = () => {
-  return (
-    <div className="container-about">
-      <UserClass name={"Aditya Class"} />
-    </div>
-  );
-};
+class About extends Component {
+  constructor(props) {
+    super(props);
+    console.log("constructor parent called");
+  }
+
+  componentDidMount() {
+    console.log("parent didMount");
+  }
+  render() {
+    console.log("render parent called");
+    return (
+      <div className="container-about">
+        <UserClass name={"Aditya Class"} />
+      </div>
+    );
+  }
+}
 
 export default About;
--- src/components/UserClass.js
+++ src/components/UserClass.js
@@ -3,11 +3,34 @@ import React from "react";
 class UserClass extends React.Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      count: 0,
+    };
+    console.log("constructor child called");
   }
+
+  componentDidMount() {
+    console.log("child didMount");
+  } 
   render() {
     const { name } = this.props;
+    let { count } = this.state;
+    console.log("render child called");
     return (
       <div>
+        <h1>Count = {count}</h1>
+        <div className="cnt-btn">
+          <button
+            onClick={() =>
+              this.setState({
+                count: this.state.count + 1,
+              })
+            }
+          >
+            increase count
+          </button>
+        </div>
         <h2>name: {name} </h2>
         <h2>location: Delhi</h2>
         <h2>Mail: way2adityakumar@gmail.com</h2>
